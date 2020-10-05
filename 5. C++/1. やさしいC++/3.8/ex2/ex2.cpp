1f5baf21d9eff27dacf4ee2ff3d9a309a93128c8
okmokm422 <okmokm422@gmail.com>
2020-10-05 23:20:06 +0900

    20201005

---
+++ 5. C++/1. やさしいC++/3.8/ex2/ex2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    char firstname;
+
+    cout << "アルファベットの最初の文字は何ですか？\n";
+
+    cin >> firstname;
+
+    cout << "アルファベットの最初の文字は" << firstname << "です。\n";
+
+    return 0;
+}
---
+++ 5. C++/1. やさしいC++/3.8/ex3/ex4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double height;
+    double weight;
+
+    cout << "身長と体重を入力してください。\n";
+
+    cin >> height >> weight;
+
+    cout << "身長は" << height << "センチです。\n";
+
+    cout << "体重は" << weight << "です。\n";
+
+    return 0;
+}
