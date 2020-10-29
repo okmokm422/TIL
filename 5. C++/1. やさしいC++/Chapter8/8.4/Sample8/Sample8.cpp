a5843bab41c754e242fa67fc4ec85a6a1cf2eb70
okmokm422 <okmokm422@gmail.com>
2020-10-29 21:59:49 +0900

    20201029

---
+++ 5. C++/1. やさしいC++/Chapter8/8.4/Sample8/Sample8.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 5;
+    int &rA = a; // 参照rAを変数aで初期化
+
+    cout << "変数aの値は" << a << "です。\n";
+    cout << "参照rAの値は" << rA << "です。\n";
+
+    rA = 50; // 参照rAに値を代入
+
+    cout << "rAに50を代入しました。\n";
+    cout << "参照rAの値は" << rA << "に変更されました。\n";
+    cout << "変数aの値も" << a << "に変更されました。\n";
+    cout << "変数aのアドレスは " << &a << "です。\n";
+    cout << "参照rAのアドレスも" << &rA << "です。\n";
+
+    return 0;
+}
