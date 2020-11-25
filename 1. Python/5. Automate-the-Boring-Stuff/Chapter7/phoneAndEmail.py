00caee3bba8995cc50f8e08f48286fb91c9735a7
okmokm422 <okmokm422@gmail.com>
2020-11-25 23:50:39 +0900

    20201125

--- 1. Python/5. Automate-the-Boring-Stuff/Chapter7/phoneAndEmail.py
+++ 1. Python/5. Automate-the-Boring-Stuff/Chapter7/phoneAndEmail.py
@@ -13,4 +13,30 @@ phone_regex = re.compile(r'''(
         (\s*(ext|x|ext.)\s(\d{2,5}))? # 内線番号
         )''', re.VERBOSE)
 
+
+# 電子メールアドレスの正規表現を作る
+email_regex = re.compile(r'''(
+        [a-zA-Z0-9._%+-]+ # ユーザー名
+        @ # @記号
+        [a-zA-Z0-9._%+-]+ # ドメイン名
+        (\.[a-zA-Z]{2,4}) # ドットなんとか
+        )''', re.VERBOSE)
+
 # クリップボードのテキストを検索する
+text = str(pyperclip.paste())
+matches = []
+for groups in phone_regex.findall(text):
+    phone_num = '-'.join([groups[1], groups[3], groups[5]])  # 市外局番、3桁の番号、4桁の番号
+    if groups[8] != '':
+        phone_num += ' x' + groups[8]  # phone_num + 内線番号
+    matches.append(phone_num)
+for groups in email_regex.findall(text):
+    matches.append(groups[0])  # emailをmatchsリストに追加
+
+# 検索結果をクリップボードに貼り付ける
+if len(matches) > 0:
+    pyperclip.copy('\n'.join(matches))
+    print('クリップボードにコピーしました。：')
+    print('\n'.join(matches))
+else:
+    print('電話番号やメールアドレスは見つかりませんでした。')
