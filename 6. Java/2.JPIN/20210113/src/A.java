33cc999e1b479295f41783d9f525cfa618944660
okmokm422 <okmokm422@gmail.com>
2021-01-25 23:29:28 +0900

    210125

---
+++ 6. Java/2.JPIN/20210113/src/A.java
@@ -0,0 +1,13 @@
+
+public class A {
+	static int num;
+	int value;
+
+	static void hello() {
+		System.out.println("static hello");
+	}
+
+	void test() {
+		System.out.println("test");
+	}
+}
---
+++ 6. Java/2.JPIN/20210113/src/Sample.java
@@ -0,0 +1,20 @@
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// staticフィールドを使う
+		// インスタンスを作らなくても使える
+		A.num = 10;
+		System.out.println(A.num);
+		// staticメソッド
+		A.hello();
+		// インスタンスを新しく作る
+		A a = new A();
+		a.value = 10;
+		a.test();
+		// スタティックでもクラス.メンバー名でコンパイルエラーにならない
+		a.num = 100;
+		a.hello();
+	}
+
+}
