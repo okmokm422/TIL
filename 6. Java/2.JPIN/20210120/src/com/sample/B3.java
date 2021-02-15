1c9efe04c46f846ef23069979623f60d27d97951
okmokm422 <okmokm422@gmail.com>
2021-02-15 23:00:21 +0900

    210215

--- 6. Java/2.JPIN/20210120/src/com/sample/B3.java
+++ 6. Java/2.JPIN/20210120/src/com/sample/B3.java
@@ -2,6 +2,13 @@ package com.sample;
 
 public class B3 extends A3 {
 
+	// 継承するけど一部のメソッドは引き継ぎたくないときはスーパークラスをOverRideする
+	@Override
+	public void hello() {
+		System.out.println("hi");
+		super.hello();
+	}
+
 	public void bye() {
 		System.out.println("bye");
 	}
